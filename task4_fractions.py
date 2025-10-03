b404fdaec118e369b8b2eac4bd0e4d88cc4c9782
0000 <a0000@MacBook-Air-0000.local>
2025-10-03 21:59:48 +0700

    Собственный итератор

---
+++ task4_fractions.py
@@ -0,0 +1,9 @@
+from fractions import Fraction
+a = Fraction(3, 4)
+b = Fraction(5, 6)
+print(f"Дробь 1: {a}")
+print(f"Дробь 2: {b}")
+print(f"Сложение: {a} + {b} = {a + b}")
+print(f"Вычитание: {a} - {b} = {a - b}")
+print(f"Умножение: {a} * {b} = {a * b}")
+print(f"Деление: {a} / {b} = {a / b}")
