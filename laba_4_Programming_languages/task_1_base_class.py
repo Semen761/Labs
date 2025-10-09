151dece3b719fbe0b8bf0fe173c2faf54fe5cf10
0000 <a0000@MacBook-Air-0000.local>
2025-10-09 12:43:58 +0300

    Добавил task_1_base_class.py в laba_4_Programming_languages

---
+++ laba_4_Programming_languages/task_1_base_class.py
@@ -0,0 +1,17 @@
+# 1. Базовый класс
+class Transport:
+    def __init__(self, brand, speed):
+        self.brand = brand
+        self.speed = speed
+    
+    def move(self):
+        print(f"Transport is moving at {self.speed} km/h")
+    
+    def __str__(self):
+        return f"Transport: {self.brand}, Speed: {self.speed}"
+
+# Проверка
+if __name__ == "__main__":
+    t = Transport("Test", 100)
+    print(t)
+    t.move()
