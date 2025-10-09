83786ec567ab0ccd4e3f22de00a478936a52a142
0000 <a0000@MacBook-Air-0000.local>
2025-10-09 14:42:08 +0300

    made class

--- laba_3_Programming_languages/task4_Custom_iterator.py
+++ laba_3_Programming_languages/task4_Custom_iterator.py
@@ -1,15 +1,24 @@
-def countdown(n):
-    current = n
-    while current >= 1:
-        yield current
-        current -= 1
+class Countdown:
+    def __init__(self, n):
+        self.n = n
+    
+    def __iter__(self):
+        self.current = self.n
+        return self
+    
+    def __next__(self):
+        if self.current < 1:
+            raise StopIteration
+        number = self.current
+        self.current -= 1
+        return number
 
 # Ввод числа от пользователя
 n = int(input("Введите число для обратного отсчета: "))
 
 # Обратный отсчет
 print(f"Обратный отсчет от {n}:")
-for x in countdown(n):
+for x in Countdown(n):
     print(x, end=" ")
 
-print() 
+print()
