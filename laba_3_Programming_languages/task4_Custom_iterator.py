723885cbdc1d17fdcfa5261cd477c976302cd301
0000 <a0000@MacBook-Air-0000.local>
2025-10-09 13:18:17 +0300

    Добавил task4_Custom_iterator.py в lab4

--- laba_3_Programming_languages/task4_Custom_iterator.py
+++ laba_3_Programming_languages/task4_Custom_iterator.py
@@ -1,24 +1,15 @@
-class Countdown:
-    def __init__(self, n):
-        self.n = n
-    
-    def __iter__(self):
-        self.current = self.n
-        return self
-    
-    def __next__(self):
-        if self.current < 1:
-            raise StopIteration
-        num = self.current
-        self.current -= 1
-        return num
+def countdown(n):
+    current = n
+    while current >= 1:
+        yield current
+        current -= 1
 
-# Вводим число с клавиатуры
-n = int(input("Введите число: "))
+# Ввод числа от пользователя
+n = int(input("Введите число для обратного отсчета: "))
 
-# Выводим все числа в одной строке
-result = []
-for x in Countdown(n):
-    result.append(str(x))
+# Обратный отсчет
+print(f"Обратный отсчет от {n}:")
+for x in countdown(n):
+    print(x, end=" ")
 
-print("Вывод:", " ".join(result))
+print() 
